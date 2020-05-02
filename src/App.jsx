b29ab2b8841c9a842c7e7865628206c6eda58e61
pickle-radish <tkdansg0222@gmail.com>
2020-05-02 17:36:45 +0900

    make basic template

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from "react";
+
+const App = () => {
+
+    return (
+        <>  
+            <div id="main_div">
+                <div id="example_img">
+                    <img src="/img/a.jpg"></img>
+                </div>
+            </div>
+            <div id="side_menu">
+                
+            </div>
+        </>
+    )
+}
+
+export default App;
