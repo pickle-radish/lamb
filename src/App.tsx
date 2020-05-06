2d2af1ca08c1e43154fb74d3101706ae4512dfdc
pickle-radish <tkdansg0222@gmail.com>
2020-05-06 14:25:59 +0900

    이미지 pdf 다운로드 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './css/example.css';
+import html2canvas from 'html2canvas';
+import jsPDF  from 'jspdf';
 
 function App() {
+    const savePDF = () => {
+
+        const printArea = document.getElementById("frame");
+        
+        html2canvas(printArea!).then(canvas => {
+            const dataURL = canvas.toDataURL();
+            const pdf = new jsPDF('p', 'mm', 'a4');
+
+            pdf.addImage(dataURL, 'JPEG', 0,0);
+            pdf.addImage(dataURL, 'JPEG', 100,0);
+
+            pdf.save('saved.pdf')
+        })
+
+    }
+
   return (
     <>  
         <div id="main_div">
@@ -21,7 +39,7 @@ function App() {
                 <input className="name form-control" type="text"></input>
             </div>
             <div>
-                <button>저장하기</button>
+                <button onClick={savePDF}>저장하기</button>
             </div>
         </div>
     </>
