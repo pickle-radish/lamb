cc650fd7f9eabb0a22d3c71dc94239f0f8c6a95b
pickle-radish <tkdansg0222@gmail.com>
2020-05-06 09:30:36 +0900

    typescript 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,31 @@
 import React from 'react';
+import logo from './logo.svg';
+import './css/example.css';
 
-const App = () => {
-
-
-    const save_img = () =>{
-
-    }
-    return (
-        <>  
-            <div id="main_div">
-                <div id="frame">
-                    <div id="example_img">
-                        <img src="/img/b.jpg"></img>   
-                        <div id="text1">나상문</div>
-                        
-                    </div>
+function App() {
+  return (
+    <>  
+        <div id="main_div">
+            <div id="frame">
+                <div id="example_img">
+                    <img src="/img/b.jpg"></img>   
+                    <div id="text1">나상문</div>
+                    
                 </div>
             </div>
-            <div id="side_menu">
-                <div className='input'>
-                    <span><label>Tag:</label></span>
-                    <span><input className='tag form-control' type="text"></input></span>
-                    <input className="name form-control" type="text"></input>
-                </div>
-                <div>
-                    <button onClick={save_img}>저장하기</button>
-                </div>
+        </div>
+        <div id="side_menu">
+            <div className='input'>
+                <span><label>Tag:</label></span>
+                <span><input className='tag form-control' type="text"></input></span>
+                <input className="name form-control" type="text"></input>
             </div>
-        </>
-    )
+            <div>
+                <button>저장하기</button>
+            </div>
+        </div>
+    </>
+  );
 }
 
 export default App;
